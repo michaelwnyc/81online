fcf5761fca3df233f25217df0019a6e728d5a3d2
michaelize <michaelmicwang@gmail.com>
2018-02-13 22:18:57 -0500

    Update instruction.php

--- instruction.php
+++ instruction.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <?php include_once('header.php'); ?>
-<title>vpn服务使用说明</title>
+<title>vpn Instructions</title>
 </head>
 <body>
 	<?php 
@@ -10,7 +10,7 @@
 	if (!(isset($_SESSION["success"]) && $_SESSION["success"] == true)) {
 		//  验证失败，将 $_SESSION["success"] 置为 false
 		$_SESSION["success"] = false;
-		echo("您没有登录,正跳转到登录页");
+		echo("You are not logged in. Redirecting to home page.");
 		echo("<meta http-equiv=refresh content='2; url=login.php'>");
 		die();
 	}
