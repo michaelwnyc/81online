e187e29ac577e41c643e23280d4bcd26ade2bfdb
michaelize <michaelmicwang@gmail.com>
2018-02-13 22:12:14 -0500

    Update change.php

--- change.php
+++ change.php
@@ -6,11 +6,11 @@
 <body>
 	<?php 
 
-	//  判断是否登陆
+	// Check if logged in.
 	if (!(isset($_SESSION["success"]) && $_SESSION["success"] === true)) {
-		//  验证失败，将 $_SESSION["success"] 置为 false
+		//  Failed, set $_SESSION["success"] to false
 		$_SESSION["success"] = false;
-		echo("您没有登录,正跳转到登录页");
+		echo("You are not logged in. Redirect to home page.");
 		echo("<meta http-equiv=refresh content='2; url=login.php'>");
 		die();
 	}
@@ -22,26 +22,26 @@
 			bgColor=#ffff99 border=0>
 			<TBODY>
 				<TR>
-					<TD align=middle height=25>原密码：&nbsp; <INPUT tabIndex=1
+					<TD align=middle height=25>Current Password：&nbsp; <INPUT tabIndex=1
 						type=password maxLength=20 size=15 name=oldpassword>
 					</TD>
 				</TR>
 				<TR>
-					<TD align=middle>新密码：&nbsp; <INPUT tabIndex=2 type=password
+					<TD align=middle>New Password：&nbsp; <INPUT tabIndex=2 type=password
 						maxLength=20 size=15 name=newpassword>
 					</TD>
 				</TR>
 				<TR>
-					<TD align=middle>重复新密码：&nbsp; <INPUT tabIndex=3 type=password
+					<TD align=middle>New Password Confirmation：&nbsp; <INPUT tabIndex=3 type=password
 						maxLength=20 size=15 name=renewpassword>
 					</TD>
 				</TR>
 				<TR>
 					<TD align=middle height=25><INPUT id=pw_manager
 						style="BORDER-RIGHT: 0px; BORDER-TOP: 0px; BORDER-LEFT: 0px; WIDTH: 65px; CURSOR: hand; BORDER-BOTTOM: 0px; HEIGHT: 18px"
-						type=submit value="修改密码" name=pw_manager> <INPUT id=reset
+						type=submit value="Change Password" name=pw_manager> <INPUT id=reset
 						style="BORDER-RIGHT: 0px; BORDER-TOP: 0px; BORDER-LEFT: 0px; WIDTH: 52px; CURSOR: hand; BORDER-BOTTOM: 0px; HEIGHT: 18px"
-						type=reset value="重置" name=reset>
+						type=reset value="Reset" name=reset>
 					</TD>
 				</TR>
 
